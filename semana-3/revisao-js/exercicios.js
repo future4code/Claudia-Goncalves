bf8d4ecc5d6a412006d6fe1c1d91277028efd564
Claudia Carvalho <claudiacarvg@gmail.com>
2021-08-23 13:35:27 -0300

    realizado exercício 3

--- semana-3/revisao-js/exercicios.js
+++ semana-3/revisao-js/exercicios.js
@@ -13,7 +13,11 @@ function retornaArrayInvertido(array) {
 }
 
 // EXERCÍCIO 03
-function retornaArrayOrdenado(array) {}
+function retornaArrayOrdenado(array) {
+  return array.sort(function (a, b) {
+    return a - b;
+  });
+}
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {}
